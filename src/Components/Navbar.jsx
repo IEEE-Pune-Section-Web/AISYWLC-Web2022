b1ebaa5ab375c48914a7176277fbaa2dca624dbf
Hunt092 <hunters.hunt92@gmail.com>
2022-06-08 23:43:13 +0530

    basic Navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
-
+import './Styles/Navbar.css'
 const Navbar = () => {
   return (
     <div className="navbar__container">
-        <nav>
+        <nav className='navbar'>
             <Link className='navbar__logo' to="/">
                 <img src="./Assets/Logo.png" alt="AISYWLC Logo" srcset="" />
             </Link>
-            <ul className="nav__links">
-                <li className="navbat__link">
+            <ul className="navbar__links">
+                <li className="navbar__link">
                     <NavLink  to='/'  >Home</NavLink>
                 </li>
-                <li className="navbat__link">
+                <li className="navbar__link">
                     <NavLink to='About'>About</NavLink>
                 </li>
-                <li className="navbat__link">
+                <li className="navbar__link">
                     <NavLink  to='Venue'>Venue</NavLink>
                 </li>
-                <li className="navbat__link">
+                <li className="navbar__link">
                     <NavLink to='Sponsor'>Sponsor</NavLink>
                 </li>
-                <li className="navbat__link">
+                <li className="navbar__link">
                     <NavLink to='Speaker'>Speakers</NavLink>
                 </li>
               
