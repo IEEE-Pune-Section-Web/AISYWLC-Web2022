295ee907984c00f6d132b743fd089b9a6fff8fc6
Hunt092 <hunters.hunt92@gmail.com>
2022-06-12 15:38:44 +0530

    Created a CTA button Component basic styling done

---
+++ src/Components/CTA_button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Styles/CTA_button.css'
+const CTA_button = ({type, icon,children}) => {
+  return (
+    <button className={type ? `cta cta__${type}` : 'cta cta__primary'}>
+            {icon ?? <div className="cta__icon">
+                {icon}</div>}
+             <div className="cta__content">
+                {children}
+             </div>
+            </button>
+  )
+}
+
+export default CTA_button
