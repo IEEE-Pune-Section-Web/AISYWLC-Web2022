dd48e5f02f87d6a7aae3655da8bfec1fe934292f
Gaurav24958 <gauravboob5@gmail.com>
2022-06-13 12:48:52 +0530

    Updated the CSS of about us section.

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -9,14 +9,12 @@ export default function About() {
     <Container className="About">
         <div className="About_content">
             <div className="About_text">
-                <h1>What we are about<br/><span className="bold_text">Transforming India for<br/>a Better Tomorrow</span></h1>
+                <h1 className="small_title">What we are about<br/><span className="bold_text">Transforming India for<br/>a Better Tomorrow</span></h1>
                 <div className="About_text_content"><br/>AISYWLC is the annual flagship event of IEEE India Council which administers a platform to young professionals, academicians, scholars, entrepreneurs and technocrats from divergent fields and careers to show up together and share a common podium.<br/><br/><br/> <span className="About_Theme">*About Theme*</span>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</div>
             </div>
 
 
-            <div className="About_video">
-                <h1>This is for video.</h1>
-            </div>
+            <div className="About_video"></div>
 
             
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from '../Components/Home/Hero'
 import About from '../Components/Home/About'
 
+
 const Home = () => {
   return (
     <main>
       <Hero/>
       <About/>
+    
     </main>
   )
 }
