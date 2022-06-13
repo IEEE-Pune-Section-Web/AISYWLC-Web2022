5ddf4d20885f944a295a715e4a8f68348784721e
Hunt092 <hunters.hunt92@gmail.com>
2022-06-13 16:06:37 +0530

    updated Nav to header in router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes  } from 'react-router-dom'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
 
 const Router = () => {
-  return (
-     <BrowserRouter> 
-     <Navbar/>
-         <Routes>
-                <Route path='/' element={<Home/>} />
-         </Routes>
-     </BrowserRouter>
-    )   
-}
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default Router
+export default Router;
