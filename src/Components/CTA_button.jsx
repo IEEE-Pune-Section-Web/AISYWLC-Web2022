b9981c01b42828650af70aeb068f98e938c34c98
Hunt092 <hunters.hunt92@gmail.com>
2022-06-13 16:05:10 +0530

    added a secondary button type to CTA; optimization

--- src/Components/CTA_button.jsx
+++ src/Components/CTA_button.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import './Styles/CTA_button.css'
-const CTA_button = ({type, icon,children}) => {
-  return (
-    <button className={type ? `cta cta__${type}` : 'cta cta__primary'}>
-            {icon ?? <div className="cta__icon">
-                {icon}</div>}
-             <div className="cta__content">
-                {children}
-             </div>
-            </button>
-  )
-}
+import React from "react";
+import "./Styles/CTA_button.css";
+const CTA_button = ({ type, icon, children }) => {
+	return (
+		<button className={type ? `cta cta__${type}` : "cta cta__primary"}>
+			{icon && <div className="cta__icon">{icon}</div>}
+			<div className="cta__content">{children}</div>
+		</button>
+	);
+};
 
-export default CTA_button
+export default CTA_button;
