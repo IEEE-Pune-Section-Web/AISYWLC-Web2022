e65b2a8173e0d7a79a872f1201c39ffcdf859c81
Hunt092 <hunters.hunt92@gmail.com>
2022-06-14 18:53:02 +0530

    Added Venue to the Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Hero from '../Components/Home/Hero'
-import About from '../Components/Home/About'
-
+import React from "react";
+import Hero from "../Components/Home/Hero";
+import About from "../Components/Home/About";
+import Venue from "../Components/Home/Venue";
 
 const Home = () => {
-  return (
-    <main>
-      <Hero/>
-      <About/>
-    
-    </main>
-  )
-}
+	return (
+		<main>
+			<Hero />
+			<About />
+			<Venue />
+		</main>
+	);
+};
 
-export default Home
+export default Home;
