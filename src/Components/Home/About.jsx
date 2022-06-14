cf6984416c8a64aee7dcfc6e8c4324a55aba95df
Hunt092 <hunters.hunt92@gmail.com>
2022-06-14 14:51:52 +0530

    About Section cleaned and made Responsive

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React from "react";
 import Container from "../../Layout/Container";
 import "./Styles/About.css";
 
-
 export default function About() {
-  return (
-    <>
-    <Container className="About">
-        <div className="About_content">
-            <div className="About_text">
-                <h1 className="small_title">What we are about<br/><span className="bold_text">Transforming India for<br/>a Better Tomorrow</span></h1>
-                <div className="About_text_content"><br/>AISYWLC is the annual flagship event of IEEE India Council which administers a platform to young professionals, academicians, scholars, entrepreneurs and technocrats from divergent fields and careers to show up together and share a common podium.<br/><br/><br/> <span className="About_Theme">*About Theme*</span>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</div>
-            </div>
+	return (
+		<>
+			<Container className="About">
+				<div className="About__content">
+					<h4 className="About__branding">What we are about</h4>
+					<h1 className="About__title">
+						Transforming India for a Better Tomorrow
+					</h1>
+					<div className="About__text">
+						<p>
+							AISYWLC is the annual flagship event of IEEE India Council which
+							administers a platform to young professionals, academicians,
+							scholars, entrepreneurs and technocrats from divergent fields and
+							careers to show up together and share a common podium.
+						</p>
+						<p>
+							<span className="About_Theme">*About Theme*</span>Class aptent
+							taciti sociosqu ad litora torquent per conubia nostra, per
+							inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac
+							scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac
+							rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi
+							convallis convallis diam sit amet lacinia. Aliquam in elementum
+							tellus.
+						</p>
+					</div>
+				</div>
 
-
-            <div className="About_video"></div>
-
-            
-        </div>
-    </Container>
-    </>
-  )
+				<div className="About__video">
+					<video src="#"></video>
+				</div>
+			</Container>
+		</>
+	);
 }
