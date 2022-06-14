45c2ff024eec7a435718cbbff36495f576d31ded
Hunt092 <hunters.hunt92@gmail.com>
2022-06-14 18:51:04 +0530

    added  OnClick to CTA buttons to add functionality

--- src/Components/CTA_button.jsx
+++ src/Components/CTA_button.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Styles/CTA_button.css";
-const CTA_button = ({ type, icon, children }) => {
+const CTA_button = ({ type, icon, onClick, children }) => {
 	return (
-		<button className={type ? `cta cta__${type}` : "cta cta__primary"}>
+		<button
+			onClick={onClick}
+			className={type ? `cta cta__${type}` : "cta cta__primary"}
+		>
 			{icon && <div className="cta__icon">{icon}</div>}
 			<div className="cta__content">{children}</div>
 		</button>
