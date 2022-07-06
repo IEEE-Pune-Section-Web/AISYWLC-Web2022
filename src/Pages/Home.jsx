e074d69ae34e3819d4181f3df85c5983935b9c91
Hunt092 <hunters.hunt92@gmail.com>
2022-07-06 22:54:52 +0530

    incomplete Ticket session pushed (styling)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "../Components/Home/Hero";
 import About from "../Components/Home/About";
 import Venue from "../Components/Home/Venue";
 import ComingSoon from "../Components/ComingSoon";
+import Tickets from "../Components/Home/Tickets";
 
 const Home = () => {
 	return (
@@ -11,6 +12,7 @@ const Home = () => {
 			<About />
 			<Venue />
 			<ComingSoon />
+			<Tickets />
 		</main>
 	);
 };
