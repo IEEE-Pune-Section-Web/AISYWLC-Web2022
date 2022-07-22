2fd2a027d19e40c44392d66d0c662c50661a43dd
Gaurav24958 <gauravboob5@gmail.com>
2022-07-23 03:53:51 +0530

    Updated Logos, Added Track Section , Removed Ticket Section , Updated CSS of speaker Section.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,18 +3,22 @@ import Hero from "../Components/Home/Hero";
 import About from "../Components/Home/About";
 import Venue from "../Components/Home/Venue";
 import ComingSoon from "../Components/ComingSoon";
-import Ticket from "../Components/Home/Ticket";
+// import Ticket from "../Components/Home/Ticket";
 import Speakers from "../Components/Home/Speakers";
+import Track from "../Components/Home/Track";
+
 
 const Home = () => {
 	return (
 		<main>
 			<Hero />
 			<About />
+			<Track/>
 			<Speakers/>
 			<Venue />
 			<ComingSoon />
-			<Ticket />
+			
+			{/* <Ticket /> */}
 			
 
 		</main>
