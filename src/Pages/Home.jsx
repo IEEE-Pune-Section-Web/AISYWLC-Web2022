dd6d3114934b890fe7eb22525fe133a7ef253a60
Hunt092 <hunters.hunt92@gmail.com>
2022-07-24 19:05:06 +0530

    decorstiont removed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,27 +9,21 @@ import Contact from "../Components/Home/Contact";
 import ContactModal from "../Components/ContactModal";
 import Track from "../Components/Home/Track";
 
-
 const Home = () => {
 	const [isModal, setModal] = useState(true);
 	return (
 		<main>
 			<Hero />
 			<About />
-			<Track/>
-			<Speakers/>
+			<Track />
+			<Speakers />
 			<Venue />
 			<ComingSoon />
-			
+
 			<Ticket />
-<<<<<<< HEAD
 			<Speakers />
 			<Contact isModal={isModal} setModal={setModal} />
 			<ContactModal isOpen={isModal} setOpen={setModal} />
-=======
-			
-
->>>>>>> 60a0a6e1c8e06d7180d3bff93956c03ccc335ddf
 		</main>
 	);
 };
