7b05e41f0511f37189ff86b6556d9cbd35a97fb4
Abishlal <abishlalns03@gmail.com>
2022-07-25 19:52:58 +0530

    tickets and tracks updated

--- src/Components/Home/Tickets/Tickets.jsx
+++ src/Components/Home/Tickets/Tickets.jsx
@@ -28,6 +28,29 @@ export default function Tickets({ type, benefits, price, link }) {
 					</div>
 				</div>
 			</div>
+			<div className="ticket">
+				<div className="left">
+					<div className="ticket-left">
+						<div className="circle"></div>
+						<div className="admit-one">
+							<span>{type ?? "IEEE Student Members"}</span>
+						</div>
+					</div>
+					<div className="ticket-info">
+						<ul>
+							{/* {benefits & benefits.map((benefit) => <li>{benefit}</li>)} */}
+							<li>Lorem Ipsum is simply dummy text of the printing an</li>
+							<li>Lorem Ipsum is simply dummy text of the printing and</li>
+							<li>Lorem Ipsum is simply dummy text of the printing and</li>
+							<li>Lorem Ipsum is simply dummy text of the printing and</li>
+						</ul>
+						<div className="price">
+							<h2>{price ?? "price"}</h2>
+						</div>
+						<CTA_button type={"primary"}>Buy Now</CTA_button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/Components/Home/Tracks/Cards.jsx
+++ src/Components/Home/Tracks/Cards.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./Styles/Cards.css";
 // import TrackDetails from '../../../Data/TrackDetails';
 
 export default function Cards(props) {
   return (
     <>
-    <div className="track_card">
+      <div className="track_card">
         <div className="track_image">
           <img src={props.image} alt="Image" srcset="" />
         </div>
 
         <div className="track_content">
-            <div className="track_head">
-                {props.name}
-            </div>
-            <div className="track_info">
-                {props.info}
-            </div>
+          <div className="track_head">{props.name}</div>
+          <div className="track_info">{props.info}</div>
         </div>
-    </div>
+        <div className="track_btn">
+          <button>Read More</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
