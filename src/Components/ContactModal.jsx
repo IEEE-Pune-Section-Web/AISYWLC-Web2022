270e968d83a0d893f3cd254abecea32b0c7b0811
Hunt092 <hunters.hunt92@gmail.com>
2022-08-01 19:51:21 +0530

    Fixed modal issue temporatily

--- src/Components/ContactModal.jsx
+++ src/Components/ContactModal.jsx
@@ -5,15 +5,56 @@ const ContactModal = ({ isOpen, setOpen }) => {
 	const [Name, setName] = useState("");
 	const [Email, setEmail] = useState("");
 	const [Msage, setMsage] = useState("");
+	function clearFields() {
+		setName("");
+		setEmail("");
+		setMsage("");
+	}
+	const scriptURL =
+		"https://script.google.com/macros/s/AKfycbxUp17NJg56jrREQVYK634Cw8zTUfKD1FkLa4ATaTvOuxB57gKEpNxC8sca2YEJXPan/exec";
+
+	const createFormJson = () => {
+		const formData = {
+			Name,
+			Email,
+			Message: Msage,
+		};
+		const data = JSON.stringify(formData);
+		console.log(data);
+		return data;
+	};
+
+	const sendRequest = (e) => {
+		e.preventDefault();
+		const form = document.forms["Contact"];
+
+		e.preventDefault();
+		fetch(scriptURL, {
+			method: "POST",
+			body: new FormData(form),
+			referrerPolicy: "origin",
+		})
+			.then((response) => {
+				console.log("Success!", response);
+				clearFields();
+				setOpen(false);
+			})
+			.catch((error) => {
+				console.error("Error!", error.message);
+				alert(
+					"There was an error, please Try again or else connect with us with our email at the footer"
+				);
+			});
+	};
 	return (
 		<div className={isOpen ? "modal__wrapper" : "modal__wrapper closed"}>
 			<div className="modal__backdrop" onClick={() => setOpen(false)}></div>
-			<div className="modal">
+			<form name="Contact" onSubmit={(e) => sendRequest(e)} className="modal">
 				<div className="modal__details">
 					<div className="modal__input">
 						<input
 							type="text"
-							name="name"
+							name="Name"
 							required
 							about="Give your name"
 							value={Name}
@@ -45,7 +86,7 @@ const ContactModal = ({ isOpen, setOpen }) => {
 				<div className="modal__button">
 					<CTA_button>Submit</CTA_button>
 				</div>
-			</div>
+			</form>
 		</div>
 	);
 };
