8764f9921bf27aea1e847905d6939e259e6f73bd
Hunt092 <hunters.hunt92@gmail.com>
2022-08-16 12:46:10 +0530

    cleaned the ticketsection

--- src/Components/Home/Ticket.jsx
+++ src/Components/Home/Ticket.jsx
@@ -3,18 +3,34 @@ import Container from "../../Layout/Container";
 import Tickets from "./Tickets/Tickets";
 import "./Styles/Ticket.css";
 
+const commonbenefits = [
+	"• Offline Session from October 7th to 9th",
+	"• Accomodation and food",
+	"• Delegate kits",
+	"• Chance to win amazing  prizes",
+	"• Increase your connect with like-minded individuals",
+];
 export default function Ticket() {
 	return (
-		<>
-			<Container className="Ticket">
-				<div className="Ticket_Header">
-					<h1 className="Ticket_title">GRAB A TICKET</h1>
-				</div>
+		<Container className="Ticket">
+			<div className="Ticket_Header">
+				<h1 className="Ticket_title">GRAB A TICKET</h1>
+			</div>
 
-				<div className="Ticket_Cards">
-					<Tickets />
-				</div>
-			</Container>
-		</>
+			<div className="Ticket_Cards">
+				<Tickets
+					type="IEEE Members"
+					benefits={commonbenefits}
+					price={4000}
+					link={"https://www.townscript.com/e/aisywlc22-340012"}
+				/>
+				<Tickets
+					type="Non-IEEE Members"
+					benefits={commonbenefits}
+					price={5000}
+					link={"https://www.townscript.com/e/aisywlc22-340012"}
+				/>
+			</div>
+		</Container>
 	);
 }
--- src/Components/Home/Tickets/Tickets.jsx
+++ src/Components/Home/Tickets/Tickets.jsx
@@ -3,56 +3,28 @@ import CTA_button from "../../CTA_button";
 import "./Styles/Tickets.css";
 
 export default function Tickets({ type, benefits, price, link }) {
+	const openLink = () => {
+		window.open(link, "_blank");
+	};
 	return (
-		<>
-			<div className="ticket">
-				<div className="left">
-					<div className="ticket-left">
-						<div className="circle"></div>
-						<div className="admit-one">
-							<span>{type ?? "IEEE Members"}</span>
-						</div>
-					</div>
-					<div className="ticket-info">
-						<ul>
-							{/* {benefits & benefits.map((benefit) => <li>{benefit}</li>)} */}
-							<li>• Offline Session from October 7th to 9th</li>
-							<li>• Accomodation and food</li>
-							<li>• Delegate kits</li>
-							<li>• Chance to win amazing  prizes</li>
-							<li>• Increase your connect with like-minded individuals</li>
-						</ul>
-						<div className="price">
-							<h2>{price ?? "₹4000"}</h2>
-						</div>
-						<CTA_button type={"primary"}><a href="https://www.townscript.com/e/aisywlc22-340012">Register Now!</a></CTA_button>
+		<div className="ticket">
+			<div className="left">
+				<div className="ticket-left">
+					<div className="circle"></div>
+					<div className="admit-one">
+						<span>{type ?? "IEEE Members"}</span>
 					</div>
 				</div>
-			</div>
-			<div className="ticket">
-				<div className="left">
-					<div className="ticket-left">
-						<div className="circle"></div>
-						<div className="admit-one">
-							<span>{type ?? "Non-IEEE Members"}</span>
-						</div>
+				<div className="ticket-info">
+					<ul>{benefits && benefits.map((benefit) => <li>{benefit}</li>)}</ul>
+					<div className="price">
+						<h2>₹{price}</h2>
 					</div>
-					<div className="ticket-info">
-						<ul>
-							{/* {benefits & benefits.map((benefit) => <li>{benefit}</li>)} */}
-							<li>• Offline Session from October 7th to 9th</li>
-							<li>• Accomodation and food</li>
-							<li>• Delegate kits</li>
-							<li>• Chance to win amazing  prizes</li>
-							<li>• Increase your connect with like-minded individuals</li>
-						</ul>
-						<div className="price">
-							<h2>{price ?? "₹5000"}</h2>
-						</div>
-						<CTA_button type={"primary"}><a href="https://www.townscript.com/e/aisywlc22-340012">Register Now!</a></CTA_button>
-					</div>
+					<CTA_button onClick={openLink} type={"primary"}>
+						Register Now!
+					</CTA_button>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
