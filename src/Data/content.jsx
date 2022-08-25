f6bca44b7c28e1a1ea64ef31fdc2d82338094b67
PG1607 <63812141+PG1607@users.noreply.github.com>
2022-08-25 10:38:21 +0530

    Update content.jsx

--- src/Data/content.jsx
+++ src/Data/content.jsx
@@ -11,7 +11,7 @@ const about = {
 
 const venue = [
 	{
-		name: "COLLEGE OF ENGINEERING PUNE, PUNE MAHARASHTRA",
+		name: "COLLEGE OF ENGINEERING PUNE",
 		pitch:
 			"College of Engineering Pune popularly known as CoEP is situated in the heart of the city. CoEP is a prestigious engineering college in Pune. CoEP, founded in 1854, is a nationally recognized leader in technical education. The institute is distinguished by its commitment to using advanced technology to solve today's major problems. The institute has a long history of striving for excellence. With a firm foundation in truth and humanity, the institute provides you with an understanding of both technical developments and the ethics that accompany them. The institute is consistently ranked among the top 20 technical colleges in India. It will be a delight to host the AISYWLC'22 at CoEP. It will offer attendees numerous opportunities for learning, networking, and growth",
 		key: "College of Engineering Pune",
