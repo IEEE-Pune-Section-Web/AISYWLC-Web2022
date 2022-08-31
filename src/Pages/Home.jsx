0294caff7a9a2c15dd908d5ba245a4fd0ccef965
PG1607 <63812141+PG1607@users.noreply.github.com>
2022-08-31 18:19:52 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
 			<Ticket />
 			<Schedule />
 			<Sponsors />
-			<FAQ />
+// 			<FAQ />
 			<Contact isModal={isModal} setModal={setModal} />
 			<ContactModal isOpen={isModal} setOpen={setModal} />
 		</main>
