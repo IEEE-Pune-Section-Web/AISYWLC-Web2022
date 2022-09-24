b947cf69ec3aac63b62bae035176ac052be4fa2f
Parv Gupta <63812141+PG1607@users.noreply.github.com>
2022-09-24 22:48:42 +0530

    Update Sponsors.jsx

--- src/Components/Home/Sponsors.jsx
+++ src/Components/Home/Sponsors.jsx
@@ -51,14 +51,9 @@ export default function Sponsors() {
 					<h2 className="sponsor_subtitle">Society Sponsors</h2>
 					<div className="Society_sopnsor">
 						<img
-							src="https://drive.google.com/uc?export=view&id=1xz3hS2U13ImQGeJukLb6jn7epht0UW4V"
+							src="https://drive.google.com/uc?export=view&id=1oxNrIgGUYoNq2dJ_HqwTb57yC9Txd84A"
 							alt="AESS-India"
-							className="Sponsor-img"
-						/>
-						<img
-							src="https://drive.google.com/uc?export=view&id=1zG1TlJs2YQ3NWlehPJu_6m2AFpG7tMJl"
-							alt="Professional Communication"
-							className="Sponsor-img"
+							className="Society"
 						/>
 					</div>
 				</div>
